6046bf61c56af1741ce136bb1c0b79882ebf6028
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-11-07 23:21:26 -0600

    sdfg
    ;laerj;

---
+++ Project/objects.js
@@ -0,0 +1,11 @@
+class drink{
+    constructor(cal, sugar, caf, cas, color)
+    {
+        this.cal = cal;
+        this.sugar = sugar;
+        this.caf = caf;
+        this.cas = cas;
+        this.color = color;
+    }
+}
+

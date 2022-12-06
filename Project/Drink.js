535c502b0df08a9709c6335b2dee5bd9f129f69a
CharlieWells04 <89938151+CharlieWells04@users.noreply.github.com>
2022-12-06 17:58:56 -0600

    sfd

--- Project/Drink.js
+++ Project/Drink.js
@@ -22,7 +22,7 @@ class Drink{
             document.getElementsByClassName("c1")[0].innerHTML = "Select A Drink";
         }
         else{
-            document.getElementsByClassName("c1")[0].innerHTML = "Calories: " + this.cal + "<br><br>Sugar: " + this.sugar + "g<br><br>Caffiene: " + this.caf + "mg<br><br>Artificial Sweetener: " + this.cas;
+            document.getElementsByClassName("c1")[0].innerHTML = "Calories: " + this.cal + "<br><br>Sugar: " + this.sugar + "g<br><br> Caffeine: " + this.caf + "mg<br><br>Artificial Sweetener: " + this.cas;
             
         }
     }
@@ -32,7 +32,7 @@ class Drink{
             document.getElementsByClassName("c2")[0].innerHTML = "Select A Drink";
         }
         else{
-            document.getElementsByClassName("c2")[0].innerHTML = "Calories: " + this.cal + "<br><br>Sugar: " + this.sugar + "g<br><br>Caffiene: " + this.caf + "mg<br><br>Artificial Sweetener: " + this.cas;
+            document.getElementsByClassName("c2")[0].innerHTML = "Calories: " + this.cal + "<br><br>Sugar: " + this.sugar + "g<br><br>Caffeine: " + this.caf + "mg<br><br>Artificial Sweetener: " + this.cas;
             
         }
     }
--- Project/Shelf.js
+++ Project/Shelf.js
@@ -13,13 +13,13 @@ class Shelf{
     Gatorade = new Drink(9, 75, 20, 0, "no", "yellow");
     GingerAle = new Drink(10, 140, 37, 0, "no", "green");
     LemonadeMinuteMaid = new Drink(11, 153, 40, 0, "no", "yellow");
-    LemonadeCharged = new Drink(12, 216, 53, 207, "no", "pink");
+    LemonadeCharged = new Drink(12, 162, 40, 155, "no", "pink");
     MilkChocolate = new Drink(13, 270, 17, 0, "no", "brown");
-    MilkSkim = new Drink(14, 116, 17, 0, "no", "white");
-    MilkWhole = new Drink(15, 211, 16, 0, "no", "white");
-    Milk2 = new Drink(16, 173, 17, 0, "no", "white");
+    MilkSkim = new Drink(14, 116, 17, 0, "no", "lightgray");
+    MilkWhole = new Drink(15, 211, 16, 0, "no", "lightgray");
+    Milk2 = new Drink(16, 173, 17, 0, "no", "lightgray");
     Monster = new Drink(17, 160, 37, 112, "no", "green");
-    MountainDew = new Drink(18, 170, 46, 54, "no", "green");
+    MountainDew = new Drink(18, 170, 46, 54, "no", "lime");
     OrangeJuice = new Drink(19, 153, 29, 0, "no", "orange");
     Pepsi = new Drink(20, 150, 41, 39, "no", "blue");
     Powerade = new Drink(21, 109, 21, "no", "blue");
@@ -37,28 +37,51 @@ class Shelf{
     
     leftCals = 0;
     rightCals = 0;
+    calTotal;
+    Lcolor;
+    RColor;
     display1(value){
         this.value = value;
         this.theList[value].displayx();
-        this.leftCals = this.theList[value].getCals;
+        this.leftCals = this.theList[value].cal;
+        this.Lcolor = this.theList[value].color;
         this.updateMiddle();
     }
     display2(value){
         this.value = value;
         this.theList[value].displayy();
-        this.rightCals = this.theList[value].getCals;
+        this.rightCals = this.theList[value].cal;
+        this.Rcolor = this.theList[value].color;
         this.updateMiddle();
     }
     updateMiddle()
     {
-        calTotal = this.leftCals + this.rightCals;
-        //calPercentLeft = this.leftCals / this.calTotal;
-        //calPercentRight = this.rightCals / this.calTotal;
-        //calPercentLeft = calPercentLeft * .88;
-        //calPercentRight = calPercentRight * .88;
-        alert("test");
-        //document.getElementsByClassName("LCal").style.width = calPercentLeft + "em";
-        //document.getElementsByClassName("RCal").style.width = calPercentRight + "em";
+        document.getElementsByClassName("LCal")[0].style.backgroundColor = this.Lcolor;
+        document.getElementsByClassName("RCal")[0].style.backgroundColor = this.Rcolor;
+        document.getElementsByClassName("t1")[0].style.backgroundColor = this.Lcolor;
+        document.getElementsByClassName("t2")[0].style.backgroundColor = this.Rcolor;
+        document.getElementsByClassName("c1")[0].style.borderColor = this.Lcolor;
+        document.getElementsByClassName("c2")[0].style.borderColor = this.Rcolor;
+        document.getElementsByClassName("t1")[0].style.borderColor = this.Lcolor;
+        document.getElementsByClassName("t2")[0].style.borderColor = this.Rcolor;
 
-     }
+        this.calTotal = this.leftCals + this.rightCals;
+        this.calPercentLeft = this.leftCals / this.calTotal;
+        this.calPercentRight = this.rightCals / this.calTotal;
+        
+        this.calPercentLeft = Math.floor(this.calPercentLeft * 100);
+        if(this.calPercentLeft == 100){
+            this.calPercentLeft = this.calPercentLeft -7;
+        }
+        if(this.calPercentRight == 100){
+            this.calPercentRight= this.calPercentRight -7;
+        }
+        this.calPercentRight = Math.floor(this.calPercentRight * 100);
+
+        document.getElementsByClassName("LCal")[0].style.width =  this.calPercentLeft +  "%";
+        document.getElementsByClassName("RCal")[0].style.width = this.calPercentRight + "%";
+        
+        
+
+    }
 }
